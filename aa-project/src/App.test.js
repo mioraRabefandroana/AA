813ae56b6c3611201b6441f10fe29a031e9dc647
miorarabefandroana <miora.rabefandroana@alumni.univ-avignon.fr>
2022-01-14 03:09:27 +0100

    Test : Page d'accueil, Connexion

--- aa-project/src/App.test.js
+++ aa-project/src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react'
 import App from './App';
+import { getUserTest, notValidUser, validUser } from './TestData';
 
-test('renders learn react link', () => {
+test("Test pour la page d'accueil", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let exploreBtn = screen.findByText(/EXPLORER/i);
+  expect(exploreBtn).not.toBeNull();
+
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Test de connexion", async () => {
+    /**
+     * Test avec un utilisateur pas valide
+     */
+    let notValidUsers = getUserTest()
+    for(let u of notValidUsers)
+    {
+        it("tentative de connexion", async ()=>{
+            render(<App />);
+            let inputUsername = document.querySelector("#login-user input")
+            let inputPassword = document.querySelector("#login-password input")
+            fireEvent.change(
+                inputUsername, 
+                {target: {value: u.username}}
+            )
+            fireEvent.change(
+                inputPassword, 
+                {target: {value: u.password}}
+            )
+
+            console.log([inputUsername.value, inputPassword.value]);
+            let loginBtn = document.querySelector(".login-connexion-btn")    
+            await fireEvent.click(loginBtn);
+
+            if(!u.success){
+                let text = "Echec d'authentification";
+                let authErrorMsgElt = await screen.findByText( new RegExp(text) );
+                expect(authErrorMsgElt).not.toBeNull();
+            }
+        })
+    }
+});
+
+
---
+++ aa-project/src/TestData.js
@@ -0,0 +1,48 @@
+// export function get_user(id=1){
+//     return {
+//         username:
+//     }
+// }
+export const notValidUser = {
+    username: "",
+    password: ""
+}
+export const validUser = {
+    username: "fan1",
+    password: "toor"
+}
+
+export function getUserTest(){
+    return [
+        // vide
+        {
+            username: "",
+            password: "",
+            success: false
+        },
+        // sans mot de passe
+        {
+            username: "userTest",
+            password: "",
+            success: false
+        },
+        // sans utilisateur
+        {
+            username: "",
+            password: "testPassword",
+            success: false
+        },
+        // un utilisateur qui n'existe pas
+        {
+            username: "userTest",
+            password: "testPassword",
+            success: false
+        },
+        // un utilisateur valide et qui existe
+        {
+            username: "fan1",
+            password: "toor",
+            success: true
+        }
+    ]
+}
